25bdc3504360e1bd090db414910333fef810bd45
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-06-30 21:52:06 +0500

    ~Meals App~ - Added new CategoryMealsScreen - The parent container widget in CategortItem doesn't contain a property for onTap so we wrap it around InkWell. We could have also wrapped around GestureDetector, but InkWell gives us a ripple effect on touch. - Navigator works like a stack where screen widgets are pushed and popped on screen. - Navigator.push takes a MaterialPageRoute object which is a wrapper that helps in providing the location of the next Widget Screen that needs to be display via builder: .
    New concepts:
    + InkWell()
      + InkWell( splashColor:)
    + Navigator()
      + Navigator.push()
        + Navigator.push(MaterialPageRoute( ))
          + Navigator.push(MaterialPageRoute( builder: ))

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -7,24 +8,37 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoryMealsScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color.withOpacity(0.7),
+                color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.withOpacity(0.7),
-              color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Recipes'),
+      ),
+      body: Center(
+        child: Text('The Recipes for the Category!'),
+      ),
+    );
+  }
+}
